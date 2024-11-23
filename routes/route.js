f7b35c7149889527d368900ee7fe2fc3fdd91e0e
sundarVCET <rajansundar@1999320@gmail.com>
2024-11-23 19:56:02 +0530

    Intial commit

---
+++ routes/route.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const studentController = require('../controllers/student_controller.js')
+
+router.post('/add/student', studentController.addStudent)
+router.get('/get/student/:rnum',studentController.getStudent)
+router.post('/addmarks/student/:rno', studentController.addMarkStudent)
+router.put('/update/student/:rno', studentController.updateStudent)
+
+
+
+module.exports = router;  // Export the router to use in app.js
+
